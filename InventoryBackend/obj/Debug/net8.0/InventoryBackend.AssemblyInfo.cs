9ea0eb5d4e6650adcb66adb005276a64d2e63145
Raveen <raveenJayasanka41@gmail.com>
2025-06-07 13:15:37 +0530

    minor

--- InventoryBackend/obj/Debug/net8.0/InventoryBackend.AssemblyInfo.cs
+++ InventoryBackend/obj/Debug/net8.0/InventoryBackend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49a20f4b44a02a095147afad2b657df4778ebd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae1eaa86342707a07990f857a18752cfc0a719c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
